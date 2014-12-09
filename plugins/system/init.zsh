03b76f6ad365bb73dda9de567afce60b192c86b8
shura <shura1991@gmail.com>
2014-12-09 02:03:12 +0100

    add completion for opam

--- plugins/system/init.zsh
+++ plugins/system/init.zsh
@@ -3,6 +3,7 @@ zmodload zsh/sched || return 1
 function system:init:completion {
   compdef _cd cdvim
   compdef _mplayer mpv
+  . /home/shura/.opam/opam-init/init.zsh > /dev/null 2> /dev/null || true
   unfunction system:init:completion
 }
 
