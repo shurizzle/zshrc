f98634104292b6c48eba8db229e54ca419e66131
Domenico Shura <shura1991@gmail.com>
2020-11-28 00:45:16 +0100

    Better integration of system plugin with sudo one

--- plugins/system/init.zsh
+++ plugins/system/init.zsh
@@ -1,14 +1,9 @@
 zmodload zsh/sched || return 1
+plugins:load-if-enabled 'sudo'
 
-function system:init:completion {
-  compdef _cd cdvim
-  compdef _mplayer mpv
-  unfunction system:init:completion
-}
-
-hooks:add zoppo_postinit system:init:completion
-
-sudo:dispatch true iotop
-sudo:dispatch true powertop
-sudo:dispatch true sg_raw
-sudo:dispatch true partprobe
+if is-function sudo:dispatch; then
+  is-command iotop && sudo:dispatch true iotop
+  is-command powertop && sudo:dispatch true powertop
+  is-command sg_raw && sudo:dispatch true sg_raw
+  is-command partprobe && sudo:dispatch true partprobe
+fi
