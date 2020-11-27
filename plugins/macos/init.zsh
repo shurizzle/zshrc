b9170fb6b9567be9dacabc19a2cefbc6cbd0d2d0
Domenico Shura <shura1991@gmail.com>
2020-11-28 00:11:58 +0100

    Remove useless if-branch

--- plugins/macos/init.zsh
+++ plugins/macos/init.zsh
@@ -17,35 +17,36 @@ if zdefault -t ':zoppo:plugin:macos:locale' enable 'yes'; then
   }
 fi
 
-if is-command brew; then
-  if is-command brew && zdefault -t ':zoppo:plugin:macos:brew' enable 'yes'; then
-    function {
-      local basepath="$1"
-      local zfunction
 
-      functions:add "$basepath"/functions/_brew
+if is-command brew && zdefault -t ':zoppo:plugin:macos:brew' enable 'yes'; then
+  function {
+    local basepath="$1"
+    local zfunction
 
-      setopt LOCAL_OPTIONS EXTENDED_GLOB BARE_GLOB_QUAL
-      for zfunction ("$basepath"/functions/_brew/^([._]|README)*(.N:t))
-        autoload -Uz -- "$zfunction"
-    } "${0:h:a}"
+    functions:add "$basepath"/functions/_brew
 
-    function {
-      local formula
-      local -a formulae
-      local formula_fn
+    setopt LOCAL_OPTIONS EXTENDED_GLOB BARE_GLOB_QUAL
+    for zfunction ("$basepath"/functions/_brew/^([._]|README)*(.N:t))
+      autoload -Uz -- "$zfunction"
+  } "${0:h:a}"
 
-      zdefault -a ':zoppo:plugin:macos:brew:gnubin:formulae' \
-        formulae formulae \
-        \
-        'coreutils' 'grep' 'gnu-tar' 'gnu-sed' 'gawk' 'make' 'openssl' \
-        'curl-openssl' 'expat' 'qt' 'openblas' 'ruby' 'apr' 'icu4c' \
-        'sqlite' 'libffi' 'openal-soft' 'util-linux' 'libpq'
+  function {
+    local formula
+    local -a formulae
+    local formula_fn
 
-      for formula in "${formulae[@]}"; do
-        formula_fn="macos:brew:formula:$formula"
-        is-function "$formula_fn" && "$formula_fn"
-      done
-    }
-  fi
+    zdefault -a ':zoppo:plugin:macos:brew:gnubin:formulae' \
+      formulae formulae \
+      \
+      'coreutils' 'grep' 'gnu-tar' 'gnu-sed' 'gawk' 'make' 'openssl' \
+      'curl-openssl' 'expat' 'qt' 'openblas' 'ruby' 'apr' 'icu4c' \
+      'sqlite' 'libffi' 'openal-soft' 'util-linux' 'libpq'
+
+    for formula in "${formulae[@]}"; do
+      formula_fn="macos:brew:formula:$formula"
+      is-function "$formula_fn" && "$formula_fn"
+    done
+  }
 fi
+
+# vim: ft=zsh sts=2 ts=2 sw=2 et fdm=marker fmr={{{,}}}
