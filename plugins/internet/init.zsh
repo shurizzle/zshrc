86162fd8b179bf6e6c164227f4072b7f51d90c49
shurizzle <me@shurizzle.dev>
2022-10-23 20:55:31 +0200

    Remove youtube-play (superseeded by mpv plugin) and using yt-dlp as a fallback

--- plugins/internet/init.zsh
+++ plugins/internet/init.zsh
@@ -2,4 +2,10 @@ zmodload zsh/net/tcp
 zmodload zsh/system
 zmodload zsh/pcre
 
-# vim: ft=zsh sts=2 ts=2 sw=2 et fdm=marker fmr={{{,}}}
+if is-command youtube-dl; then
+  alias ytdl=youtube-dl
+elif is-command yt-dlp; then
+  alias ytdl=yt-dlp
+fi
+
+# vim: ft=zsh sts=2 ts=2 sw=2 et fdm=marker fmr=
